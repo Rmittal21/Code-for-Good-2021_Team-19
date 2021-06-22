390c06be91f529cf17fd0715b5990e836f1ac9c6
Roopal <54892036+Rmittal21@users.noreply.github.com>
2021-06-22 19:46:59 +0530

    Create Facilitator.js

---
+++ src/components/Facilitator.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import App from "./facilitatorApp";
+
+export default class Facilitator extends Component {
+  render() {
+    return (
+      <App />
+    )
+  }
+}
