1283435f0c907741c6ba62c5e8a511c7653ee177
Roopal <54892036+Rmittal21@users.noreply.github.com>
2021-06-22 19:46:34 +0530

    Create FormData.js

---
+++ src/components/FormData.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class FormData extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Form</h2>
+      </div>
+    )
+  }
+}
