c512d62a8d69edbfd9e424fed4dcaab45f0b03ef
Roopal <54892036+Rmittal21@users.noreply.github.com>
2021-06-22 19:46:16 +0530

    Create School.js

---
+++ src/components/School.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './school.css';
+import App from "./schoolApp";
+export default class School extends Component {
+  render() {
+    return (
+      <App />
+    )
+  }
+}
