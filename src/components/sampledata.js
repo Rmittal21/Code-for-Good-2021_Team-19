534a138df8acbcf1cd37d809cdd9d2d599dd0c33
Roopal <54892036+Rmittal21@users.noreply.github.com>
2021-06-22 19:46:46 +0530

    Create sampledata.js

---
+++ src/components/sampledata.js
@@ -0,0 +1,27 @@
+export const sampledata=[
+    {
+        "School Name": "abc",
+        "Facilitator Name":"def",
+        "week": "Mon",
+        "language": "hindi"
+    },
+    {
+        "School Name": "abc",
+        "Facilitator Name":"def",
+        "week": "Mon",
+        "language": "hindi"
+    },
+    {
+        "School Name": "abc",
+        "Facilitator Name":"def",
+        "week": "Mon",
+        "language": "hindi"
+    },
+    {
+        "School Name": "abc",
+        "Facilitator Name":"def",
+        "week": "Mon",
+        "language": "hindi"
+    },
+
+]
